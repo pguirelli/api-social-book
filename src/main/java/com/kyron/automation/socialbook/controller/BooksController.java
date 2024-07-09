71103f9a975fad76c656be014aa1af26c21474d6
Publio Buiatti Guirelli <publiotester@gmail.com>
2024-07-09 20:39:14 -0300

    :sparkles: Creating an endpoint to get book by id

--- src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
+++ src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
@@ -1,8 +1,10 @@
 package com.kyron.automation.socialbook.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +28,11 @@ public class BooksController {
     @RequestMapping(method = RequestMethod.POST)
     public void save(@RequestBody Book book) {
         booksRepository.save(book);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<Book> getBook(@PathVariable("id") Long id) {
+        return booksRepository.findById(id);
     }
 
 }
