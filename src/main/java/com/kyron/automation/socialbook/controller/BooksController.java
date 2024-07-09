91cf52ecb80ed76baac6d4c9069e05505599f633
Publio Buiatti Guirelli <publiotester@gmail.com>
2024-07-09 20:30:50 -0300

    :sparkles: Creating an endpoint to save books

--- src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
+++ src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
@@ -3,6 +3,7 @@ package com.kyron.automation.socialbook.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,20 @@ import com.kyron.automation.socialbook.model.Book;
 import com.kyron.automation.socialbook.repository.BooksRepository;
 
 @RestController
+@RequestMapping("/books")
 public class BooksController {
 
     @Autowired
     private BooksRepository booksRepository;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public List<Book> list() {
         return booksRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void save(@RequestBody Book book) {
+        booksRepository.save(book);
     }
 
 }
