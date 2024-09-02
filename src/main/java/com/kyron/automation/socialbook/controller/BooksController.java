a6f3ed89a07797014e5bf5ef0006f45910668de2
Publio Buiatti Guirelli <publiotester@gmail.com>
2024-09-02 20:31:10 -0300

    :recycle: Correct treatment of http responses 404 and 201

--- src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
+++ src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
@@ -1,14 +1,18 @@
 package com.kyron.automation.socialbook.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kyron.automation.socialbook.model.Book;
 import com.kyron.automation.socialbook.repository.BooksRepository;
@@ -26,13 +30,23 @@ public class BooksController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public void save(@RequestBody Book book) {
+    public ResponseEntity<Void> save(@RequestBody Book book) {
         booksRepository.save(book);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(book.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Optional<Book> getBook(@PathVariable("id") Long id) {
-        return booksRepository.findById(id);
+    public ResponseEntity<?> getBook(@PathVariable("id") Long id) {
+        Optional<Book> book = booksRepository.findById(id);
+
+        if (book.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(book);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
