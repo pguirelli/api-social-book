0eea4e46534f6bf701339298e78073bc599333c2
Publio Buiatti Guirelli <publiotester@gmail.com>
2024-09-10 18:43:57 -0300

    :heavy_plus_sign: Adding jackson-dataformat to POM.xml to accept xml in media type

--- src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
+++ src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,7 +27,8 @@ public class BooksController {
     @Autowired
     private BooksService booksService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<List<Book>> list() {
         return ResponseEntity.status(HttpStatus.OK).body(booksService.list());
     }
