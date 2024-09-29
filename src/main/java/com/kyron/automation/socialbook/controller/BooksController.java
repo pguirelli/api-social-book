28b32bc6a9ca2581954686387e9b7938c5f55ea2
Publio Buiatti Guirelli <publiotester@gmail.com>
2024-09-29 15:20:32 -0300

    :sparkles: Adding CrossOrigin annotation in book controller

--- src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
+++ src/main/java/com/kyron/automation/socialbook/controller/BooksController.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ public class BooksController {
     @Autowired
     private BooksService booksService;
 
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<List<Book>> list() {
